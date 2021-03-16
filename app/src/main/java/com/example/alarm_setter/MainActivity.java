5b2976542b6de5df60a189b7f2813e90bb1a8dc0
Babitababy <kumaribabita2402@gmail.com>
2021-03-16 14:29:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/alarm_setter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alarm_setter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
