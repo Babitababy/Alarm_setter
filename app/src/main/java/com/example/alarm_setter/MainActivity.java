f504a492e47876227428863a3063591c7a15e6a8
Babitababy <kumaribabita2402@gmail.com>
2021-03-20 10:40:48 +0530

    UI changed

--- app/src/main/java/com/example/alarm_setter/MainActivity.java
+++ app/src/main/java/com/example/alarm_setter/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.alarm_setter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 import java.util.Calendar;
+import java.util.Date;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     TimePicker time_Picker;
-    AlarmManager manager;
-    PendingIntent pending_intent;
+    TextView textView;
+    int mHour,mMin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,45 +26,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         time_Picker = (TimePicker) findViewById(R.id.time_picker);
-        manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        textView=(TextView)findViewById(R.id.time_textview);
 
-    }
-    public void OnToggleClicked(View view)
-    {
-        long alarm_time;
+        time_Picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                mHour=hourOfDay;
+                mMin=minute;
+                textView.setText(textView.getText().toString()+" "+mHour+":"+mMin);
 
-        if (((ToggleButton) view).isChecked())
-        {
 
-            Toast.makeText(MainActivity.this, "ALARM ON", Toast.LENGTH_SHORT).show();
-            Calendar calendar = Calendar.getInstance();
-            //get current hour
-            calendar.set(Calendar.HOUR_OF_DAY, time_Picker.getCurrentHour());
-            //get ccurrent minute
-            calendar.set(Calendar.MINUTE, time_Picker.getCurrentMinute());
-            Intent intent = new Intent(this, AlarmReceiver.class);
-            pending_intent = PendingIntent.getBroadcast(this, 0, intent, 0);
-
-            alarm_time=(calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
-            if(System.currentTimeMillis()>alarm_time)
-            {
-                if (calendar.AM_PM == 0)
-                    alarm_time  = alarm_time + (1000*60*60*12);
-                else
-                    alarm_time = alarm_time + (1000*60*60*24);
             }
-            //alarm is repeated until it is turned off by the uer
-            manager.setRepeating(AlarmManager.RTC_WAKEUP, alarm_time, 10000, pending_intent);
+        });
 
-        }
-
-        else
-        {
-            manager.cancel(pending_intent);
-            //alarm off
-            Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_LONG).show();
-        }
 
     }
+
+    //Now we will implement method to implement alarm time
+    public void setTimer(View v)
+    {
+        //Now we are going to use AlarmManager Class
+      AlarmManager alarmManager=(AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Date date = new Date();
+        Calendar cal_Alarm = Calendar.getInstance();//this cal_Alarm
+        // object is for alarm calendar time
+
+        Calendar cal_now = Calendar.getInstance(); //this cal_now
+        // object is for current time
+
+        cal_now.setTime(date);
+        cal_Alarm.setTime(date);
+
+        //Now set Hour,Minute and second
+        cal_Alarm.set(Calendar.HOUR_OF_DAY,mHour);
+        cal_Alarm.set(Calendar.MINUTE,mMin);
+        cal_Alarm.set(Calendar.SECOND,0);
+
+        //check if cal_alarm is before cal_now
+        if(cal_Alarm.before(cal_now))
+        {
+            cal_Alarm.add(Calendar.DATE,1);
+        }
+        //now we will simply call BroadcastReceiver
+
+
+
+
+    }
+
+
 }
 
